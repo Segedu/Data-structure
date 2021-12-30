084bf8564791005af17a5f73debc0b27105a6754
Segedu <seggedu20@gmail.com>
2021-12-30 19:14:17 +0200

    30.12.2021

--- homework/30.12.2021/main.js
+++ homework/30.12.2021/main.js
@@ -12,13 +12,28 @@
 // console.log(sumNumValues(333, 0))
 
 //!2 works only if the array is sorted
-function findMinInArray(array, min) {
-    if (array[1] < min) {
-        min = array[1]
-        array.shift()
-        return (findMinInArray(array, min))
+// function findMinInArray(array, min) {
+//     if (array[1] < min) {
+//         min = array[1]
+//         array.shift()
+//         return (findMinInArray(array, min))
+//     }
+//     return min
+// }
+// const array = [7, 4, 3, 2, -2, -20];
+// console.log(findMinInArray(array, array[0]));
+
+//!3
+function insertToArray(count, array) {
+    if (count < 3) {
+        const userInput = Number(prompt("enter number"));
+        if (userInput !== 0 && userInput !== " " && userInput == userInput) {
+            array.push(userInput);
+            count++;
+        }
+        return insertToArray(count, array);
     }
-    return min
+    return array
 }
-const array = [7, 4, 3, 2, -2, -20];
-console.log(findMinInArray(array, array[0]));
+const array = [];
+console.log(insertToArray(0, array))
