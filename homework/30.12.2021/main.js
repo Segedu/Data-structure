1df5af61ef32864ebfec3f1b7287095c1d525ca0
Segedu <seggedu20@gmail.com>
2022-01-02 09:32:58 +0200

    30.12.2021

--- homework/30.12.2021/main.js
+++ homework/30.12.2021/main.js
@@ -1,14 +1,14 @@
 //!1
-// function sumNumValues(number, total) {
-//     if (number > 0) {
-//         total += (number % 10);
-//         let divideByTen = number / 10
-//         let updatedNum = parseInt(divideByTen)
-//         return sumNumValues(updatedNum, total)
-//     }
-//     return total
-// }
-// console.log(sumNumValues(333, 0));
+function sumNumValues(number, total) {
+    if (number > 0) {
+        total += (number % 10);
+        let divideByTen = number / 10;
+        let updatedNum = parseInt(divideByTen);
+        return sumNumValues(updatedNum, total);
+    }
+    return total;
+}
+console.log(sumNumValues(135, 0));
 
 //!2 works only if the array is sorted
 // function findMinInArray(array, min) {
